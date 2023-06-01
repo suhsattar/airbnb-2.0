269ae07f7c8e13a4dacaaad70217b9a25a029aab
Suhayb Sattar <suhaybsattar@Suhaybs-MBP.attlocal.net>
2023-05-31 21:16:23 -0500

    dynamic auto

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
-export const dynamic = "force-dynamic";
+export const dynamic = "auto";
 
 interface HomeProps {
   searchParams: IListingsParams;
